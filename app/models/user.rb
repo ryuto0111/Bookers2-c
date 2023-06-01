ee2e8e8fc7eb18856e57b3e2084ca0ab922b5342
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-06-01 08:20:54 +0000

    [Add]グループ機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,11 @@ class User < ApplicationRecord
   has_many :user_rooms
   has_many :chats
   has_many :rooms, through: :user_rooms
+  has_many :group_user
+  has_many :groups, through: :group_users
   has_many :view_counts, dependent: :destroy
-
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   resources :chats, only: [:show, :create]
+  resources :groups, expect: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
