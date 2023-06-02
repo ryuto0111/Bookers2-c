fc6863310e6cdb4b5ddacbb09c3fb62c071b658e
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-06-02 05:48:22 +0000

    [Save]

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,21 +1,21 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
-  
+
   def index
     @book = Book.new
     @groups = Group.all
   end
-  
+
   def show
     @book = Book.new
     @group = Group.find(params[:id])
   end
-  
+
   def new
     @group = Group.new
   end
-  
+
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
@@ -25,24 +25,24 @@ class GroupsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @group.update(group_params)
       redirect_to groups_path
     else
-      render 'edit'
+      render "edit"
     end
   end
-  
+
   private
-  
+
   def group_params
-    @arams.require(:group).premit(:name, :introduction, :image)
+    params.require(:group).permit(:name, :introduction, :image)
   end
-  
+
   def ensure_correct_user
     @group = Group.find(params[:id])
     unless @group.owner_id == current_user.id
@@ -50,5 +50,4 @@ class GroupsController < ApplicationController
     end
   end
   
-
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,12 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
-  
+
   validates :name, presence: true
   validates :introduction, presence: true
   has_one_attached :image
+
+  def get_group_image
+    (group_image.attached?) ? group_image : 'no_image.jpg'
+  end
 end
