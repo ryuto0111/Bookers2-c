e0bb7d185454737d35689def86cddf5e6df3dfd6
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-06-03 12:01:15 +0000

    [Save]

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,8 @@ Rails.application.routes.draw do
   resources :chats, only: [:show, :create]
   resources :groups, only:[:index, :show, :edit, :create, :update, :new, :show] do
     resource :group_users, only: [:create, :destroy]
-    get "new/mail" => "groups#new_mail"
-    get "send/mail" => "groups#send_mail"
+    resources :event_notices, only: [:new, :create]
+    get "event_notices" => "event_notices#sent"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
