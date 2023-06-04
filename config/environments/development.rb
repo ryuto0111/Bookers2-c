dc401a84394d6b5d2592060077e5960e27be5c09
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-06-04 01:40:51 +0000

    [Save]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,7 +74,7 @@ Rails.application.configure do
   config.hosts.clear
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
@@ -85,5 +85,5 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto:  true
   }
-  
+
 end
